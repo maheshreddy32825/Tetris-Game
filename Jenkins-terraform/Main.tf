85d92ed699f20682b1fff4667974b0c8218a64fd
mahesh.amireddy <maheshreddyedu32825@gmail.com>
2024-09-11 08:47:20 -0500

    config changes

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -58,9 +58,9 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-03f4878755434977f"
+  ami                    = "ami-0182f373e66f89c85"
   instance_type          = "t2.large"
-  key_name               = "Argo key"
+  key_name               = "mahesh-aws"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
