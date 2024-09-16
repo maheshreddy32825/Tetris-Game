921bea78ac2f3219be8355a0333d6051fc18d6df
mahesh.amireddy <maheshreddyedu32825@gmail.com>
2024-09-16 16:57:13 -0500

    eks terraform

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mario123bucket" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
-    region = "ap-south-1"
+    bucket = "my-aws-statefile-bucket" # Replace with your actual S3 bucket name
+    key    = "EKS/eks-terraform.tfstate"
+    region = "us-east-1"
   }
 }
